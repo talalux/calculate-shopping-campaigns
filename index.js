667974a9e754ab6022df503b78a805fdf03aad3f
talalux <talalux.n@gmail.com>
2025-05-29 23:20:59 +0700

    add fuction submit cal discount

--- index.js
+++ index.js
@@ -6,36 +6,6 @@ document.addEventListener("DOMContentLoaded", function(e) {
     buttonSubmit();
 });
 
-function fixDiscount(
-    {
-        discountAmout,
-        discountPercentage,
-        point,
-        arrItems
-    }
-){
-    let itemsInCart = [
-        {
-            title: "T-Shirt",
-            amount: 1,
-            price: 350
-        },
-        {
-            title: "Hat",
-            amount: 1,
-            price: 250
-        },
-    ]
-
-    let sum = itemsInCart.reduce((a, b) => {
-        let sumPrice = (a.amount * a.price) + (b.amount * b.price);
-        return sumPrice;
-    })
-    let totalPrice = sum - parseFloat(discountAmout)
-    console.log('1. fix discount '+ discountAmout + ' THB');
-    console.log('total price: '+ totalPrice + " THB");
-}
-
 function appendInput(){
     return new Promise((resolve) => {
         let itemsInCart = [
@@ -47,7 +17,7 @@ function appendInput(){
             {
                 title: "Hat",
                 price: 250,
-                category: "Clothing"
+                category: "Accessories"
             },
             {
                 title: "Hoodie",
@@ -80,7 +50,7 @@ function appendInput(){
                         inputHTML += '<span class="price_items">price: '+items.price+' THB</span>'
                     inputHTML += '</div>'
                     inputHTML += '<span>amount: </span>'
-                    inputHTML += '<input type="text" data-price="'+items.price+'" data-title="'+items.title+'" data-category="'+items.category+'">'
+                    inputHTML += '<input data-input="number" type="text" data-price="'+items.price+'" data-title="'+items.title+'" data-category="'+items.category+'">'
                 inputHTML += '</div>'
             inputHTML += '</div>'
         })
@@ -90,7 +60,7 @@ function appendInput(){
 }
 
 function inputNumber(){
-    var findInput = document.querySelectorAll("input[data-category]");
+    var findInput = document.querySelectorAll("input[data-input='number']");
     var arrIncludeNumber = [
         "0",
         "1",
@@ -130,10 +100,87 @@ function buttonSubmit(){
                 sumPrice: parseInt(getAmout !== "" ? getAmout : 0) * parseInt(getPrice)
             }
         })
-        htmlCartDetail(dataValue).then(data => {
-            var findTargetAppendDetail = document.querySelector('.items_in_cart')
-            findTargetAppendDetail.innerHTML = data;
-        })
+        dataValue = dataValue.filter(data => data.amount !== 0);
+        var findTargetAppendDetail = document.querySelector('.items_in_cart')
+        if(dataValue.length > 0){
+            htmlCartDetail(dataValue).then(data => {
+                findTargetAppendDetail.innerHTML = data;
+            });
+        }else{
+            findTargetAppendDetail.innerHTML = "No data.";
+            return;
+        }
+        var arrSum = dataValue.map(data => data.sumPrice);
+        var totalPrice = arrSum.reduce((a,b) => (a + b))
+        var totalDiscount = 0;
+        console.log(totalPrice);
+
+        var main = document.querySelector('.main_cart')
+        // percentage with items category
+        var findAppendValuePercentageWithCategoryDiscount = main.querySelector('[data-campaigns="percentage_discount_category"]')
+        var getDataCateClothing = dataValue.filter(data => data.category == "Clothing");
+        var totalClothingPrice = getDataCateClothing.map(data => data.sumPrice);
+        totalClothingPrice = totalClothingPrice.reduce((a,b) => (a+b));
+        var discountPercentageWithCategory = parseInt(totalClothingPrice * 15) / 100;
+        totalDiscount += discountPercentageWithCategory;
+        findAppendValuePercentageWithCategoryDiscount.innerHTML = discountPercentageWithCategory;
+        totalPrice = (totalPrice - discountPercentageWithCategory);
+
+        // fixed disoucnt
+        var findInputFixedDiscount = main.querySelector('.fixed_discount');
+        var findAppendValueFixedDiscount = main.querySelector('[data-campaigns="fixed_discount"]')
+        if(findInputFixedDiscount.value !== ""){
+            findAppendValueFixedDiscount.innerHTML = findInputFixedDiscount.value;
+            totalPrice = totalPrice - parseInt(findInputFixedDiscount.value)
+            totalDiscount +=  parseFloat(findInputFixedDiscount.value)
+            console.log(findInputFixedDiscount.value, 'from fixed');
+        }else{
+            findAppendValueFixedDiscount.innerHTML =0;
+        }
+    
+        // percentage
+        var findInputPercentageDiscount = main.querySelector('.percentage_discount');
+        var findAppendValuePercentageDiscount = main.querySelector('[data-campaigns="percentage_discount"]');
+        if(findInputPercentageDiscount.value !== ""){
+            var findDiscountValueByPercentage = (totalPrice * parseInt(findInputPercentageDiscount.value)) / 100
+            findAppendValuePercentageDiscount.innerHTML = findDiscountValueByPercentage;
+            totalPrice = totalPrice - findDiscountValueByPercentage
+            totalDiscount +=  parseFloat(findDiscountValueByPercentage)
+            console.log(findDiscountValueByPercentage, 'from percen');
+        }else{
+            findAppendValuePercentageDiscount.innerHTML = 0
+        }
+        
+        // discount with point
+        var findInputPoint = main.querySelector('.poin_discount')
+        var findAppendValueDiscountPoint = main.querySelector('[data-campaigns="discount_point"]');
+        if(findInputPoint.value !== ""){
+            var findMaxDiscount = parseInt(totalPrice * 0.2);
+            var discountValue = parseInt(findInputPoint.value) <= findMaxDiscount ? parseInt(findInputPoint.value) : findMaxDiscount
+            totalPrice = totalPrice - discountValue;
+            findAppendValueDiscountPoint.innerHTML = discountValue
+            totalDiscount +=  parseFloat(discountValue)
+            console.log(discountValue, 'from point');
+        }else{
+            findAppendValueDiscountPoint.innerHTML = 0
+        }
+
+        // special
+        var findSpecialDiscount = parseInt(totalPrice/300);
+        var specialDiscountValue = findSpecialDiscount * 40;
+        var findAppendValueDiscountSpecial = main.querySelector('[data-campaigns="special_campaigns"]');
+        if(specialDiscountValue !== 0){
+            findAppendValueDiscountSpecial.innerHTML = specialDiscountValue
+            totalDiscount +=  parseFloat(specialDiscountValue)
+            totalPrice = totalPrice - specialDiscountValue;
+        }else{
+            findAppendValueDiscountSpecial.innerHTML = 0
+        }
+
+        var findAppendTotalDiscount = main.querySelector("[data-total='discount']");
+        findAppendTotalDiscount.innerHTML = totalDiscount;
+        var findAppendTotalPrice = main.querySelector("[data-total='price']");
+        findAppendTotalPrice.innerHTML = totalPrice;
     })
 }
 
